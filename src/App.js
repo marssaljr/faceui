a79ed990848eca34d4a3ec1a382e58602fd08938
marssaljr <marssaljr@gmail.com>
2021-07-01 04:46:47 +0000

    🚀 feat: create a element for each data

--- src/App.js
+++ src/App.js
@@ -24,11 +24,17 @@ export default function App() {
   if (error) {
     return 'Error!' + error;
   }
+  const results = data.results;
+  // to see all data from scheme see: <pre>{JSON.stringify(data, null, 2)}</pre>
   return (
     <>
-      <h1>ladrão de galinha</h1>
-      <img src={data.results[0].picture.medium} alt="random user"/>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h1>{results[0].name.first} {results[0].name.last}</h1>
+      <img src={results[0].picture.medium} alt="random user"/>
+      <p>gender: {results[0].gender}</p>
+      <p>age: {results[0].dob.age}</p>
+      <p>from: {results[0].location.city}, {results[0].location.country}</p>
+      <p>email: {results[0].email}</p>
+      <p>cell: {results[0].cell}</p>
     </>
   );
 }
