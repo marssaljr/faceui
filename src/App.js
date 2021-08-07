553e87d6f019a5115818165d12aa8a6d8fa0d0b6
marssaljr <marssaljr@gmail.com>
2021-08-07 02:19:21 +0000

    👌 refactor: change ui style

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
         });
   }, []);
   if (loading) {
-    return 'Loading...';
+    return (<h1 id='loading'>Loading...</h1>);
   }
   if (error) {
     return 'Error!' + error;
@@ -36,15 +36,17 @@ export default function App() {
   // to see all data from scheme see: <pre>{JSON.stringify(data, null, 2)}</pre>
   return (
     <>
-      <div className="head">
-        <h1>{results[0].name.first} {results[0].name.last}</h1>
-        <img src={results[0].picture.medium} alt="random user"/>
+      <div className="celphone">
+        <div className="head">
+          <img src={results[0].picture.medium} alt="random user"/>
+          <h1>{results[0].name.first} {results[0].name.last}</h1>
+        </div>
+        <p><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> {Gender(gender)}</p>
+        <p><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg> {results[0].dob.age}</p>
+        <p><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> {location}</p>
+        <p><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg> {results[0].email}</p>
+        <p><svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg> {results[0].cell}</p>
       </div>
-      <p>gender: {Gender(gender)}</p>
-      <p>age: {results[0].dob.age}</p>
-      <p>from: {location}</p>
-      <p>email: {results[0].email}</p>
-      <p>cell: {results[0].cell}</p>
     </>
   );
 }
